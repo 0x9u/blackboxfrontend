4d6d1ff9d542f6f71f99725d2d7db2a26f50f219
snOliver Po <91490@churchie.com.au>
2022-05-27 19:29:47 +1000

    6th commit

--- src/components/chat.js
+++ src/components/chat.js
@@ -5,15 +5,36 @@ function Chat() {
     return (
         <div className="chat-container">
                 <div className="menu">
+                    <div className="menu-option">
+                        <p>Brah</p>
+                        </div>
+                        <div className="menu-option">
+                        </div>
+                        <div className="menu-option">
+                        </div>
+                        <div className="menu-option">
+                        </div>
+                        <div className="menu-option">
+                        </div>
+
                     </div>
                 <div className="chat">
                     <div className="chat-content">
                         <div className="msg">
-                            <p>Hello!</p>
+                            <img src="https://fanbuzz.com/wp-content/uploads/sites/5/2019/05/Dwayne-Johnson.png" width="50" height="50" alt="pfp"/>
+                            <p><span>The rock</span>Hello!</p>
+                        </div>
+                        <div className="msg">
+                        <img src="https://fanbuzz.com/wp-content/uploads/sites/5/2019/05/Dwayne-Johnson.png" width="50" height="50" alt="pfp"/>
+                            <p><span>The rock</span>Test 123</p>
                         </div>
                     </div>
                     <div className="chat-control">
+                        <div className="user-input">
+                        <textarea placeholder="type here!"/>
+                        <input type="button" value="Send!"/>
                         </div>
+                    </div>
                 </div>
         </div>
     );
